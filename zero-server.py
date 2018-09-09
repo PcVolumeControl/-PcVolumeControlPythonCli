d523b5305ae02a71d5adff18fd0dca2cd1c4236b
Bill Booth <moronbros@gmail.com>
2018-09-09 14:05:41 -0700

    Change to argparse and clean up code
    This makes the utility slightly easier to use. It should probably still
    take in all the options for the mDNS ServiceInfo object.

--- zero-server.py
+++ zero-server.py
@@ -10,40 +10,34 @@ Just use the IP and port your server is advertising.
 Even more data can be embedded in the properties of the ServiceInfo object.
 """
 
+import argparse
 import logging
 import socket
-import sys
 from time import sleep
-
 from zeroconf import ServiceInfo, Zeroconf
 
-THE_SERVER_IP = sys.argv[1]
-THE_SERVER_PORT = int(sys.argv[2])
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    if len(sys.argv) > 3:
-        assert sys.argv[3:] == ['--debug']
+def main(parsed_args):
+    """Advertise a service on the local subnet using mDNS."""
+
+    # set the log level based on the verbosity they need.
+    logging.basicConfig(level=logging.INFO)
+    if parsed_args.verbose:
         logging.getLogger('zeroconf').setLevel(logging.DEBUG)
 
-    # docs:
-    # https://github.com/jstasiak/python-zeroconf/blob/master/zeroconf.py#L1363
-
-
-    desc = {'missiles': 'armed'}
+    portnum = int(parsed_args.port)
 
     info = ServiceInfo(type_="_http._tcp.local.",
                        name="pcvolumecontrol._http._tcp.local.",
-                       address=socket.inet_aton(THE_SERVER_IP),
-                       port=THE_SERVER_PORT,
+                       address=socket.inet_aton(parsed_args.ip),
+                       port=portnum,
                        weight=0,
                        priority=0,
-					   properties=desc,
-					   server="myservername.local.")
-
+                       properties={'missiles': 'armed'},
+                       server="myservername.local.")
 
     zeroconf = Zeroconf()
-    print("Registration of a service, press Ctrl-C to exit...")
+    print("Service is now registered at: {}:{}, press Ctrl-C to exit...".format(parsed_args.ip, portnum))
     zeroconf.register_service(info)
     try:
         while True:
@@ -53,4 +47,15 @@ if __name__ == '__main__':
     finally:
         print("Unregistering...")
         zeroconf.unregister_service(info)
-zeroconf.close()
+        zeroconf.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Advertise a service on the LAN using mDNS.')
+    parser.add_argument('ip', action='store', help='IP address of the service')
+    parser.add_argument('port', action='store', help='Port of the service')
+    parser.add_argument('--verbose', '-v', action='store_true')
+
+    args = parser.parse_args()
+
+    main(args)
